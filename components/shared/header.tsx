a66fcb245eadf6db2f0828496fe910af06a59851
Vadim Cherepanov <xenos26@gmail.com>
2024-10-17 21:58:26 +0700

    feat: add right side of header in components

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -1,5 +1,7 @@
 import { Container } from "@/components/shared/container";
+import { Button } from "@/components/ui";
 import { cn } from "@/lib/utils";
+import { ArrowRight, ShoppingCart, User } from "lucide-react";
 import Image from "next/image";
 import React from "react";
 
@@ -19,6 +21,27 @@ export const Header: React.FC<Props> = ({ className }) => {
             <p className="text-sm text-gray-400 leading-3">
               вкусней уже некуда
             </p>
+          </div>
+        </div>
+        {/* Правая часть */}
+        <div className="flex items-center gap-3">
+          <Button variant="outline" className="flex items-center gap-1">
+            <User size={16} />
+            Войти
+          </Button>
+          <div>
+            <Button className="group relative">
+              <b>520 ₽</b>
+              <span className="h-full w-[1px] bg-white/30 mx-3" />
+              <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
+                <ShoppingCart className="relative" size={16} strokeWidth={2} />
+                <b>3</b>
+              </div>
+              <ArrowRight
+                className="absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0"
+                size={20}
+              />
+            </Button>
           </div>
         </div>
       </Container>
