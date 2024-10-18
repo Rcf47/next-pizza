eaaf543dd1eb065bde477db75beed9ab03b4366f
Vadim Cherepanov <xenos26@gmail.com>
2024-10-18 22:26:15 +0700

    feat: add filters on main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Container } from "@/components/shared/container";
+import { Filters } from "@/components/shared/filters";
 import { Title } from "@/components/shared/title";
 import { TopBar } from "@/components/shared/top-bar";
 
@@ -7,7 +8,17 @@ export default function Home() {
     <>
       <Container className="mt-10">
         <Title text="Все пиццы" size="lg" className="font-extrabold" />
-        <TopBar />
+      </Container>
+      <TopBar />
+      <Container className="pb-14">
+        <div className="flex gap-[60px]">
+          {/* Фильтрация */}
+          <div className="w-[250px]">
+            <Filters />
+          </div>
+          {/* Список товаров */}
+          <div className="flex flex-col gap-16">Cписок товаров</div>
+        </div>
       </Container>
     </>
   );
