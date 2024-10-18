b219b7f68ca0fccacdbd4cee94ec00385693d084
Vadim Cherepanov <xenos26@gmail.com>
2024-10-18 22:26:04 +0700

    feat: add filters component

---
+++ components/shared/filters.tsx
@@ -0,0 +1,19 @@
+import { FilterCheckbox } from "@/components/shared/filter-checkbox";
+import { Title } from "@/components/shared/title";
+import React from "react";
+
+interface Props {
+  className?: string;
+}
+
+export const Filters: React.FC<Props> = ({ className }) => {
+  return (
+    <div className={className}>
+      <Title text="Фильтрация" size="sm" className="mb-5 font-bold" />
+      <div className="flex flex-col gap-4">
+        <FilterCheckbox text="Можно собирать" value="1" />
+        <FilterCheckbox text="Новинки" value="2" />
+      </div>
+    </div>
+  );
+};
