4acd7e198a72baa22094e0a7f889acd97d716391
Vadim Cherepanov <xenos26@gmail.com>
2024-10-18 22:02:15 +0700

    feat: change top-bar on main page with component top-bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
-import { Categories } from "@/components/shared/categories";
 import { Container } from "@/components/shared/container";
-import { SortPopup } from "@/components/shared/sort-popup";
 import { Title } from "@/components/shared/title";
+import { TopBar } from "@/components/shared/top-bar";
 
 export default function Home() {
   return (
     <>
       <Container className="mt-10">
         <Title text="Все пиццы" size="lg" className="font-extrabold" />
-        <Categories />
-        <SortPopup />
+        <TopBar />
       </Container>
     </>
   );
