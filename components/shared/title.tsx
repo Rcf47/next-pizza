2b3d8c0286975e3a1b4434f9213447660a306a01
Vadim Cherepanov <xenos26@gmail.com>
2024-10-18 20:34:13 +0700

    feat: add title.tsx component

--- components/shared/title.tsx
+++ components/shared/title.tsx
@@ -1,3 +1,6 @@
+import clsx from "clsx";
+import React from "react";
+
 type TitleSize = "xs" | "sm" | "md" | "lg" | "xl" | "2xl";
 
 interface Props {
@@ -5,3 +8,29 @@ interface Props {
   className?: string;
   text: string;
 }
+
+export const Title: React.FC<Props> = ({ text, size = "sm", className }) => {
+  const mapTagBySize = {
+    xs: "h5",
+    sm: "h4",
+    md: "h3",
+    lg: "h2",
+    xl: "h1",
+    "2xl": "h1",
+  } as const;
+
+  const mapClassNameBySize = {
+    xs: "text-[16px] ys-text",
+    sm: "text-[22px] ys-text",
+    md: "text-[28px] ys-text",
+    lg: "text-[32px] ys-display",
+    xl: "text-[40px] ys-display",
+    "2xl": "text-[48px] ys-display",
+  } as const;
+
+  return React.createElement(
+    mapTagBySize[size],
+    { className: clsx(mapClassNameBySize[size], className) },
+    text
+  );
+};
