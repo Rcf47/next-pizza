2768e98a024571b98d17e0fae03c4ac881acb59a
Vadim Cherepanov <xenos26@gmail.com>
2024-10-22 23:35:50 +0700

    feat: add useIntersection in products-group

--- components/shared/products-group-list.tsx
+++ components/shared/products-group-list.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useIntersection } from "react-use";
 import { ProductCard } from "@/components/shared/product-card";
 import { Title } from "@/components/shared/title";
 import { cn } from "@/lib/utils";
@@ -18,8 +21,18 @@ export const ProductsGroupList: React.FC<Props> = ({
   className,
   listClassName,
 }) => {
+  const intersectionRef = React.useRef(null);
+  const intersection = useIntersection(intersectionRef, {
+    threshold: 0.4,
+  });
+
+  React.useEffect(() => {
+    if (intersection?.isIntersecting) {
+      console.log(title, categoryId);
+    }
+  }, [intersection, categoryId, title]);
   return (
-    <div className={className}>
+    <div className={className} id={title} ref={intersectionRef}>
       <Title text={title} size="lg" className="font-extrabold mb-5" />
       <div className={cn("grid grid-cols-3 gap-[50px]", listClassName)}>
         {items.map((product) => (
