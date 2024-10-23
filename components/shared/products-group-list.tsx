1038d70060cdc8543d9472ee385891d82564c026
Vadim Cherepanov <xenos26@gmail.com>
2024-10-23 22:30:58 +0700

    feat: set active id in product group list

--- components/shared/products-group-list.tsx
+++ components/shared/products-group-list.tsx
@@ -5,6 +5,7 @@ import { ProductCard } from "@/components/shared/product-card";
 import { Title } from "@/components/shared/title";
 import { cn } from "@/lib/utils";
 import React from "react";
+import { useCategoryStore } from "@/store/category";
 
 interface Props {
   categoryId: number;
@@ -21,6 +22,7 @@ export const ProductsGroupList: React.FC<Props> = ({
   className,
   listClassName,
 }) => {
+  const setActiveCategoryId = useCategoryStore((state) => state.setActiveId);
   const intersectionRef = React.useRef(null);
   const intersection = useIntersection(intersectionRef, {
     threshold: 0.4,
@@ -28,9 +30,9 @@ export const ProductsGroupList: React.FC<Props> = ({
 
   React.useEffect(() => {
     if (intersection?.isIntersecting) {
-      console.log(title, categoryId);
+      setActiveCategoryId(categoryId);
     }
-  }, [intersection, categoryId, title]);
+  }, [intersection, setActiveCategoryId, categoryId]);
   return (
     <div className={className} id={title} ref={intersectionRef}>
       <Title text={title} size="lg" className="font-extrabold mb-5" />
