95357b51df7a1890bac40611546f70902556f083
Vadim Cherepanov <xenos26@gmail.com>
2024-11-16 07:08:50 +0700

    feat: add prisma/seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,28 @@
+import { Prisma } from "@prisma/client";
+import { categories, ingredients, products } from "../prisma/constants";
 import { prisma } from "../prisma/prisma-client";
 import { hashSync } from "bcrypt";
+
+const randomDecimalNumber = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min) * 10 + min * 10) / 10;
+};
+
+const generatePizza = ({
+  productId,
+  pizzaType,
+  size,
+}: {
+  productId: number;
+  pizzaType?: 1 | 2;
+  size?: 20 | 30 | 40;
+}) => {
+  return {
+    productId,
+    price: randomDecimalNumber(190, 600),
+    pizzaType,
+    size,
+  } as Prisma.ProductItemUncheckedCreateInput;
+};
 
 async function generateData() {
   await prisma.user.createMany({
@@ -25,6 +48,65 @@ async function generateData() {
         verified: new Date(),
         role: "ADMIN",
       },
+    ],
+  });
+  await prisma.category.createMany({
+    data: categories,
+  });
+  await prisma.ingredient.createMany({
+    data: ingredients,
+  });
+  await prisma.product.createMany({
+    data: products,
+  });
+  const pizza1 = await prisma.product.create({
+    data: {
+      name: "Пепперони фреш",
+      imageUrl:
+        "https://media.dodostatic.net/image/r:233x233/11EE7D61304FAF5A98A6958F2BB2D260.webp",
+      categoryId: 1,
+      ingredients: {
+        connect: ingredients.slice(0, 5),
+      },
+    },
+  });
+  const pizza2 = await prisma.product.create({
+    data: {
+      name: "Сырная",
+      imageUrl:
+        "https://media.dodostatic.net/image/r:233x233/11EE7D610CF7E265B7C72BE5AE757CA7.webp",
+      categoryId: 1,
+      ingredients: {
+        connect: ingredients.slice(5, 10),
+      },
+    },
+  });
+  const pizza3 = await prisma.product.create({
+    data: {
+      name: "Чоризо фреш",
+      imageUrl:
+        "https://media.dodostatic.net/image/r:584x584/11EE7D61706D472F9A5D71EB94149304.webp",
+      categoryId: 1,
+      ingredients: {
+        connect: ingredients.slice(10, 40),
+      },
+    },
+  });
+
+  //Сделал как обычные объекты, так и через функцию для практики
+  await prisma.productItem.createMany({
+    data: [
+      generatePizza({ productId: 1, pizzaType: 1, size: 20 }),
+      generatePizza({ productId: 1, pizzaType: 2, size: 30 }),
+      generatePizza({ productId: 1, pizzaType: 2, size: 40 }),
+
+      generatePizza({ productId: 2, pizzaType: 1, size: 20 }),
+      generatePizza({ productId: 2, pizzaType: 2, size: 30 }),
+      generatePizza({ productId: 2, pizzaType: 2, size: 40 }),
+
+      generatePizza({ productId: 3, pizzaType: 1, size: 20 }),
+      generatePizza({ productId: 3, pizzaType: 2, size: 30 }),
+      generatePizza({ productId: 3, pizzaType: 2, size: 40 }),
     ],
   });
 }
