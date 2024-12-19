b1bc33eef649bc29d1209e814766faccdc398968
Vadim Cherepanov <xenos26@gmail.com>
2024-12-20 00:14:16 +0700

    feat: add render list for query in search-input component

--- components/shared/search-input.tsx
+++ components/shared/search-input.tsx
@@ -2,6 +2,7 @@
 
 import { cn } from "@/lib/utils";
 import { Api } from "@/services/api-client";
+import { Product } from "@prisma/client";
 import { Search } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
@@ -63,26 +64,31 @@ export const SearchInput: React.FC<Props> = ({ className }) => {
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
         />
-        <div
-          className={cn(
-            "absolute w-full bg-white rounded-xl py-2 top-14 shadow-md transition-all duration-200 invisible opacity-0 z-30",
-            focused && "visible opacity-100 top-12"
-          )}
-        >
-          <Link
-            className="flex items-center gap-3 w-full px-3 py-2 hover:bg-primary/10 cursor-pointer"
-            href="/product/1"
+        {products.length > 0 && (
+          <div
+            className={cn(
+              "absolute w-full bg-white rounded-xl py-2 top-14 shadow-md transition-all duration-200 invisible opacity-0 z-30",
+              focused && "visible opacity-100 top-12"
+            )}
           >
-            <Image
-              className="rounded-sm h-8 w-8"
-              src="/pizza/pizza-1.avif"
-              alt="Пицца 1"
-              width={32}
-              height={32}
-            />
-            <span>Пицца 1</span>
-          </Link>
-        </div>
+            {products.map((product) => (
+              <Link
+                key={product.id}
+                className="flex items-center gap-3 w-full px-3 py-2 hover:bg-primary/10 cursor-pointer"
+                href={`/product/${product.id}`}
+              >
+                <Image
+                  className="rounded-sm h-8 w-8"
+                  src={product.imageUrl}
+                  alt={product.name}
+                  width={32}
+                  height={32}
+                />
+                <span>{product.name}</span>
+              </Link>
+            ))}
+          </div>
+        )}
       </div>
     </>
   );
