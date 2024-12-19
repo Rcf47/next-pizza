4ecae376fdf6e9eebd6c17d84451beaa25cb7ee5
Vadim Cherepanov <xenos26@gmail.com>
2024-12-19 23:05:14 +0700

    feat: add search function for api request

--- services/products.ts
+++ services/products.ts
@@ -1,1 +1,10 @@
-export const search = async (query: string) => {};
+import { axiosInstance } from "@/services/axios-instance";
+import { Product } from "@prisma/client";
+
+export const search = async (query: string) => {
+  const { data } = await axiosInstance.get<Product>("/products/search", {
+    params: { query },
+  });
+
+  return data;
+};
