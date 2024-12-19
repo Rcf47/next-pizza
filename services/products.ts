744766ad52772fc4e7b6622755805ae1e5b81f70
Vadim Cherepanov <xenos26@gmail.com>
2024-12-20 00:12:59 +0700

    fix: fix wrong product response from services/products

--- services/products.ts
+++ services/products.ts
@@ -2,10 +2,13 @@ import { ApiRoutes } from "@/services/api-routes";
 import { axiosInstance } from "@/services/axios-instance";
 import { Product } from "@prisma/client";
 
-export const search = async (query: string) => {
-  const { data } = await axiosInstance.get<Product>(ApiRoutes.SEARCH_PRODUCTS, {
-    params: { query },
-  });
+export const search = async (query: string): Promise<Product[]> => {
+  const { data } = await axiosInstance.get<Product[]>(
+    ApiRoutes.SEARCH_PRODUCTS,
+    {
+      params: { query },
+    }
+  );
 
   return data;
 };
