d7cbb3f082a0085c67d92ed2e0be5039147e25e8
Vadim Cherepanov <xenos26@gmail.com>
2025-01-04 17:30:43 +0700

    fix: fix search on all filter groups

--- components/shared/checkbox-filters-group.tsx
+++ components/shared/checkbox-filters-group.tsx
@@ -66,13 +66,15 @@ export const CheckboxFiltersGroup: React.FC<Props> = ({
   return (
     <div className={className}>
       <p className="font-bold mb-3">{title}</p>
-      <div className="mb-5">
-        <Input
-          onChange={onChangeSearchInput}
-          placeholder={searchInputPlaceholder}
-          className="bg-gray-50 border-none"
-        />
-      </div>
+      {showAll && (
+        <div className="mb-5">
+          <Input
+            onChange={onChangeSearchInput}
+            placeholder={searchInputPlaceholder}
+            className="bg-gray-50 border-none"
+          />
+        </div>
+      )}
       <div className="flex flex-col gap-4 max-h-96 pr-2 overflow-auto scrollbar">
         {list.map((item) => (
           <FilterCheckbox
