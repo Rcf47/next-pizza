8aba3cf680f61671ca496492c80e9f276eda81ba
Vadim Cherepanov <xenos26@gmail.com>
2025-01-07 17:18:59 +0700

    feat: add database request for main page

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,21 @@ import { Filters } from "@/components/shared/filters";
 import { ProductsGroupList } from "@/components/shared/products-group-list";
 import { Title } from "@/components/shared/title";
 import { TopBar } from "@/components/shared/top-bar";
+import { ingredients } from "@/prisma/constants";
+import { prisma } from "@/prisma/prisma-client";
 
-export default function Home() {
+export default async function Home() {
+  const categories = await prisma.category.findMany({
+    include: {
+      products: {
+        include: {
+          ingredients: true,
+          items: true,
+        },
+      },
+    },
+  });
+  console.log(categories[1]);
   return (
     <>
       <Container className="mt-10">
@@ -18,103 +31,20 @@ export default function Home() {
             <Filters />
           </div>
           {/* Список товаров */}
-          <div className="flex flex-col gap-16">
-            <ProductsGroupList
-              title="Пиццы"
-              items={[
-                {
-                  id: 1,
-                  name: "Мясная-пицца",
-                  imageUrl: "/pizza/pizza-1.avif",
-                  price: 390,
-                  items: [{ price: 390 }],
-                },
-                {
-                  id: 2,
-                  name: "Мясная-пицца",
-                  imageUrl: "/pizza/pizza-1.avif",
-                  price: 390,
-                  items: [{ price: 390 }],
-                },
-                {
-                  id: 3,
-                  name: "Мясная-пицца",
-                  imageUrl: "/pizza/pizza-1.avif",
-                  price: 390,
-                  items: [{ price: 390 }],
-                },
-                {
-                  id: 4,
-                  name: "Мясная-пицца",
-                  imageUrl: "/pizza/pizza-1.avif",
-                  price: 390,
-                  items: [{ price: 390 }],
-                },
-                {
-                  id: 5,
-                  name: "Мясная-пицца",
-                  imageUrl: "/pizza/pizza-1.avif",
-                  price: 390,
-                  items: [{ price: 390 }],
-                },
-                {
-                  id: 6,
-                  name: "Мясная-пицца",
-                  imageUrl: "/pizza/pizza-1.avif",
-                  price: 390,
-                  items: [{ price: 390 }],
-                },
-              ]}
-              categoryId={0}
-            />
-            <ProductsGroupList
-              title="Завтрак"
-              items={[
-                {
-                  id: 7,
-                  name: "Мясная-пицца",
-                  imageUrl: "/pizza/pizza-1.avif",
-                  price: 390,
-                  items: [{ price: 390 }],
-                },
-                {
-                  id: 8,
-                  name: "Мясная-пицца",
-                  imageUrl: "/pizza/pizza-1.avif",
-                  price: 390,
-                  items: [{ price: 390 }],
-                },
-                {
-                  id: 9,
-                  name: "Мясная-пицца",
-                  imageUrl: "/pizza/pizza-1.avif",
-                  price: 390,
-                  items: [{ price: 390 }],
-                },
-                {
-                  id: 10,
-                  name: "Мясная-пицца",
-                  imageUrl: "/pizza/pizza-1.avif",
-                  price: 390,
-                  items: [{ price: 390 }],
-                },
-                {
-                  id: 11,
-                  name: "Мясная-пицца",
-                  imageUrl: "/pizza/pizza-1.avif",
-                  price: 390,
-                  items: [{ price: 390 }],
-                },
-                {
-                  id: 12,
-                  name: "Мясная-пицца",
-                  imageUrl: "/pizza/pizza-1.avif",
-                  price: 390,
-                  items: [{ price: 390 }],
-                },
-              ]}
-              categoryId={1}
-            />
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">
+              {categories.map(
+                (category) =>
+                  category.products.length > 0 && (
+                    <ProductsGroupList
+                      key={category.id}
+                      title={category.name}
+                      categoryId={category.id}
+                      items={category.products}
+                    />
+                  )
+              )}
+            </div>
           </div>
         </div>
       </Container>
