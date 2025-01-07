5b7e1d84ee374569f1d3306f10f5e2b143616e6b
Vadim Cherepanov <xenos26@gmail.com>
2025-01-07 17:19:20 +0700

    chore: temporarly comment price field in main page

--- components/shared/products-group-list.tsx
+++ components/shared/products-group-list.tsx
@@ -43,7 +43,7 @@ export const ProductsGroupList: React.FC<Props> = ({
             id={product.id}
             name={product.name}
             imageUrl={product.imageUrl}
-            price={product.items[0].price}
+            //price={product.items[0].price}
           />
         ))}
       </div>
