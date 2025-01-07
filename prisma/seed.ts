11bc0462b254b7f0f1188b29cbf99d623f50988e
Vadim Cherepanov <xenos26@gmail.com>
2025-01-07 17:18:05 +0700

    refactor: delete pizza const from seed prisma

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -59,39 +59,6 @@ async function generateData() {
   await prisma.product.createMany({
     data: products,
   });
-  const pizza1 = await prisma.product.create({
-    data: {
-      name: "Пепперони фреш",
-      imageUrl:
-        "https://media.dodostatic.net/image/r:233x233/11EE7D61304FAF5A98A6958F2BB2D260.webp",
-      categoryId: 1,
-      ingredients: {
-        connect: ingredients.slice(0, 5),
-      },
-    },
-  });
-  const pizza2 = await prisma.product.create({
-    data: {
-      name: "Сырная",
-      imageUrl:
-        "https://media.dodostatic.net/image/r:233x233/11EE7D610CF7E265B7C72BE5AE757CA7.webp",
-      categoryId: 1,
-      ingredients: {
-        connect: ingredients.slice(5, 10),
-      },
-    },
-  });
-  const pizza3 = await prisma.product.create({
-    data: {
-      name: "Чоризо фреш",
-      imageUrl:
-        "https://media.dodostatic.net/image/r:584x584/11EE7D61706D472F9A5D71EB94149304.webp",
-      categoryId: 1,
-      ingredients: {
-        connect: ingredients.slice(10, 40),
-      },
-    },
-  });
 
   //Сделал как обычные объекты, так и через функцию для практики
   await prisma.productItem.createMany({
