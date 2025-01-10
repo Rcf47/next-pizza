9451c312c6778d9f843795fbe4914352c19c2036
Vadim Cherepanov <xenos26@gmail.com>
2025-01-10 16:53:00 +0700

    feat: add group router in next js

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "../globals.css";
 import { Nunito, Roboto_Mono } from "next/font/google";
 import { Header } from "@/components/shared/header";
 
