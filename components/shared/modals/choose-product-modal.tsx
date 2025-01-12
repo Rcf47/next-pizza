fc502fdc38485e8fd627610e900cf7a225a331f0
Vadim Cherepanov <xenos26@gmail.com>
2025-01-12 18:16:00 +0700

    feat: add isPizzaFrom check in modal window

--- components/shared/modals/choose-product-modal.tsx
+++ components/shared/modals/choose-product-modal.tsx
@@ -1,20 +1,21 @@
 "use client";
 
+import { ProductWithRelations } from "@/@types/product";
 import { ChooseProductForm } from "@/components/shared/choose-product-form";
 import { Dialog } from "@/components/ui";
 import { DialogContent } from "@/components/ui/dialog";
 import { cn } from "@/lib/utils";
-import { Product } from "@prisma/client";
 import { useRouter } from "next/navigation";
 import React from "react";
 
 interface Props {
-  product: Product;
+  product: ProductWithRelations;
   className?: string;
 }
 
 export const ChooseProductModal: React.FC<Props> = ({ product, className }) => {
   const router = useRouter();
+  const isPizzaForm = Boolean(product.items[0].pizzaType);
 
   return (
     <Dialog open={Boolean(product)} onOpenChange={() => router.back()}>
@@ -24,11 +25,15 @@ export const ChooseProductModal: React.FC<Props> = ({ product, className }) => {
           className
         )}
       >
-        <ChooseProductForm
-          imageUrl={product.imageUrl}
-          name={product.name}
-          ingredients={[]}
-        />
+        {isPizzaForm ? (
+          "PizzaFrom"
+        ) : (
+          <ChooseProductForm
+            imageUrl={product.imageUrl}
+            name={product.name}
+            ingredients={[]}
+          />
+        )}
       </DialogContent>
     </Dialog>
   );
