7f1126799ba04f6d189c564f13ffc574af0d892b
Vadim Cherepanov <xenos26@gmail.com>
2025-01-13 18:21:36 +0700

    chore: add pizza types constant

--- shared/constants/pizza.ts
+++ shared/constants/pizza.ts
@@ -1,5 +1,10 @@
-const mapSize = {
+const mapPizzaSize = {
   20: "Маленькая",
   30: "Средняя",
   40: "Большая",
+} as const;
+
+const mapPizzaType = {
+  1: "Тонкое",
+  2: "Традиционное",
 } as const;
