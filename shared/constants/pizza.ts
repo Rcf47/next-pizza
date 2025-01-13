90d00be3851ec28192ad70b211b1c384a1f204c5
Vadim Cherepanov <xenos26@gmail.com>
2025-01-13 18:23:48 +0700

    chore: add pizzasizes array from constant

--- shared/constants/pizza.ts
+++ shared/constants/pizza.ts
@@ -1,10 +1,15 @@
-const mapPizzaSize = {
+export const mapPizzaSize = {
   20: "Маленькая",
   30: "Средняя",
   40: "Большая",
 } as const;
 
-const mapPizzaType = {
+export const mapPizzaType = {
   1: "Тонкое",
   2: "Традиционное",
 } as const;
+
+export const pizzaSizes = Object.entries(mapPizzaSize).map(([name, value]) => ({
+  name,
+  value,
+}));
