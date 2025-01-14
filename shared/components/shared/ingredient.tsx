f6342f08bd77ec490165f58f718edecc70ace47e
Vadim Cherepanov <xenos26@gmail.com>
2025-01-14 19:17:19 +0700

    feat: finish ingredient component

--- shared/components/shared/ingredient.tsx
+++ shared/components/shared/ingredient.tsx
@@ -1,3 +1,6 @@
+import { cn } from "@/shared/lib/utils";
+import { CircleCheck } from "lucide-react";
+import Image from "next/image";
 import React from "react";
 
 interface Props {
@@ -9,6 +12,29 @@ interface Props {
   onClick?: () => void;
 }
 
-export const Ingredient: React.FC<Props> = ({ className }) => {
-  return <div className={className}></div>;
+export const Ingredient: React.FC<Props> = ({
+  className,
+  imageUrl,
+  name,
+  price,
+  active,
+  onClick,
+}) => {
+  return (
+    <div
+      className={cn(
+        "flex items-center flex-col p-1 rounded-md w-32 text-center relative cursor-pointer shadow-md bg-white",
+        { "border border-primary": active },
+        className
+      )}
+      onClick={onClick}
+    >
+      {active && (
+        <CircleCheck className="absolute top-2 right-2 text-primary" />
+      )}
+      <Image width={110} height={110} src={imageUrl} alt="ingredient" />
+      <span className="text-xs mb-1">{name}</span>
+      <span className="font-bold">{price} ₽</span>
+    </div>
+  );
 };
