bd99bdc71715a170acb69936f0792b90e86ebd59
Vadim Cherepanov <xenos26@gmail.com>
2025-01-18 00:11:58 +0700

    chore: add type for get-available-pizza-sizes

--- shared/lib/get-available-pizza-sizes.ts
+++ shared/lib/get-available-pizza-sizes.ts
@@ -1,10 +1,11 @@
+import { Variant } from "@/shared/components/shared/group-variants";
 import { PizzaType, pizzaSizes } from "@/shared/constants/pizza";
 import { ProductItem } from "@prisma/client";
 
 export const getAvailablePizzaSizes = (
   type: PizzaType,
   items: ProductItem[]
-) => {
+): Variant[] => {
   const filteredPizzasByType = items.filter((item) => item.pizzaType === type);
   const availablePizzaSizes = pizzaSizes.map((item) => ({
     name: item.name,
