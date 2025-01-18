812a71a4b3c5f8b6bc68754cda18ddb380e07e92
Vadim Cherepanov <xenos26@gmail.com>
2025-01-18 22:22:53 +0700

    feat: add cart drawer component

--- shared/components/shared/cart-button.tsx
+++ shared/components/shared/cart-button.tsx
@@ -1,3 +1,4 @@
+import { CartDrawer } from "@/shared/components/shared/cart-drawer";
 import { Button } from "@/shared/components/ui";
 import { cn } from "@/shared/lib/utils";
 import { ArrowRight, ShoppingCart } from "lucide-react";
@@ -9,17 +10,19 @@ interface Props {
 
 export const CartButton: React.FC<Props> = ({ className }) => {
   return (
-    <Button className={cn(className, "group relative")}>
-      <b>520 ₽</b>
-      <span className="h-full w-[1px] bg-white/30 mx-3" />
-      <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
-        <ShoppingCart className="relative" size={16} strokeWidth={2} />
-        <b>3</b>
-      </div>
-      <ArrowRight
-        className="absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0"
-        size={20}
-      />
-    </Button>
+    <CartDrawer>
+      <Button className={cn(className, "group relative")}>
+        <b>520 ₽</b>
+        <span className="h-full w-[1px] bg-white/30 mx-3" />
+        <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
+          <ShoppingCart className="relative" size={16} strokeWidth={2} />
+          <b>3</b>
+        </div>
+        <ArrowRight
+          className="absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0"
+          size={20}
+        />
+      </Button>
+    </CartDrawer>
   );
 };
