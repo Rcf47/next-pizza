a1d29b57a3b7a829a9abddd4e6c6c41de6bec90f
Vadim Cherepanov <xenos26@gmail.com>
2025-01-20 23:35:57 +0700

    feat(cart-drawer-item): add cart-drawer-item html elements

--- shared/components/shared/cart-drawer-item.tsx
+++ shared/components/shared/cart-drawer-item.tsx
@@ -2,6 +2,8 @@ import { cn } from "@/shared/lib/utils";
 import React from "react";
 import * as CartItem from "./cart-item-details";
 import { CartItemProps } from "@/shared/components/shared/cart-item-details/cart-item-details.types";
+import { CountButton } from "@/shared/components/shared/count-button";
+import { Trash2Icon } from "lucide-react";
 
 interface Props extends CartItemProps {
   className?: string;
@@ -22,6 +24,17 @@ export const CartDrawerItem: React.FC<Props> = ({
       <CartItem.Image src={imageUrl} />
       <div className="flex-1">
         <CartItem.Info name={name} details={details} />
+        <hr className="my-3" />
+        <div className="flex items-center justify-between">
+          <CountButton onClick={(type) => console.log(type)} value={quantity} />
+          <div className="flex items-center gap-3">
+            <CartItem.Price value={price} />
+            <Trash2Icon
+              className="text-gray-400 cursor-pointer hover:text-gray-600"
+              size={16}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
