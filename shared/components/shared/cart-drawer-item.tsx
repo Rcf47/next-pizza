035707743cba839efbd7ff5f529a5f6738677962
Vadim Cherepanov <xenos26@gmail.com>
2025-01-20 23:03:23 +0700

    feat(cart-drawer-item): start working on cart-drawer-item

--- shared/components/shared/cart-drawer-item.tsx
+++ shared/components/shared/cart-drawer-item.tsx
@@ -1,9 +1,28 @@
+import { cn } from "@/shared/lib/utils";
 import React from "react";
+import * as CartItem from "./cart-item-details";
+import { CartItemProps } from "@/shared/components/shared/cart-item-details/cart-item-details.types";
 
-interface Props {
+interface Props extends CartItemProps {
   className?: string;
 }
 
-export const CartDrawerItem: React.FC<Props> = ({ className }) => {
-  return <div className={className}></div>;
+export const CartDrawerItem: React.FC<Props> = ({
+  id,
+  imageUrl,
+  details,
+  name,
+  price,
+  quantity,
+  disabled,
+  className,
+}) => {
+  return (
+    <div className={cn("flex bg-white p-5 gap-6", className)}>
+      <CartItem.Image src={imageUrl} />
+      <div className="flex-1">
+        <CartItem.Info name={name} details={details} />
+      </div>
+    </div>
+  );
 };
