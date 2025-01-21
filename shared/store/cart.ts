242c375f4dfbc42350f48f9727cfc886812cdede
Vadim Cherepanov <xenos26@gmail.com>
2025-01-21 23:02:35 +0700

    chore(store): add cart store code

--- shared/store/cart.ts
+++ shared/store/cart.ts
@@ -1,4 +1,6 @@
-import { CartStateItem } from "@/shared/lib/get-cart-details";
+import { CartStateItem, getCartDetails } from "@/shared/lib/get-cart-details";
+import { Api } from "@/shared/services/api-client";
+import { create } from "zustand";
 
 export interface CartState {
   loading: boolean;
@@ -13,8 +15,35 @@ export interface CartState {
   updateItemQuantity: (id: number, quantity: number) => Promise<void>;
 
   // Запрос на добавление товара в корзину
-  addCartItem: (values: CreateCartItemValues) => Promise<void>;
+  // TODO: Типизировать values
+  addCartItem: (values: any) => Promise<void>;
 
   // Запрос на удаление товара из корзины
   removeCartItem: (id: number) => Promise<void>;
 }
+
+export const useCartStore = create<CartState>((set, get) => ({
+  items: [],
+  error: false,
+  loading: true,
+  totalAmount: 0,
+
+  fetchCartItems: async () => {
+    try {
+      set({ loading: true, error: false });
+      const data = await Api.cart.getCart();
+      set(getCartDetails(data));
+    } catch (error) {
+      console.error(error);
+      set({ error: true });
+    } finally {
+      set({ loading: false });
+    }
+  },
+
+  updateItemQuantity: async (id: number, quantity: number) => {},
+
+  removeCartItem: async (id: number) => {},
+
+  addCartItem: async (values: any) => {},
+}));
