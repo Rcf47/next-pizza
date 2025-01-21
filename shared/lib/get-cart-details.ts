ffd91eb472438977c849297db231359886a663cc
Vadim Cherepanov <xenos26@gmail.com>
2025-01-21 23:39:08 +0700

    feat(lib): add get-cart-details function

--- shared/lib/get-cart-details.ts
+++ shared/lib/get-cart-details.ts
@@ -1,3 +1,5 @@
+import { CartDTO } from "@/shared/services/dto/cart.dto";
+
 export type CartStateItem = {
   id: number;
   quantity: number;
@@ -10,4 +12,28 @@ export type CartStateItem = {
   ingredients: Array<{ name: string; price: number }>;
 };
 
-export const getCartDetails = (data: any) => {};
+interface ReturnProps {
+  items: CartStateItem[];
+  totalAmount: number;
+}
+export const getCartDetails = (data: CartDTO): ReturnProps => {
+  const items = data.items.map((item) => ({
+    id: item.id,
+    quantity: item.quantity,
+    name: item.productItem.product.name,
+    imageUrl: item.productItem.product.imageUrl,
+    price: calcCartItemTotalPrice(),
+    pizzaSize: item.productItem.size,
+    pizzaType: item.productItem.pizzaType,
+    disabled: false,
+    ingredients: item.ingredients.map((ingredient) => ({
+      name: ingredient.name,
+      price: ingredient.price,
+    })),
+  }));
+
+  return {
+    items,
+    totalAmount: data.totalAmount,
+  };
+};
