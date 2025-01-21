4e7979e9524415d3e3e11587c87d7c33eb8e3d56
Vadim Cherepanov <xenos26@gmail.com>
2025-01-21 22:47:40 +0700

    feat(store): add type for cart in store

---
+++ shared/store/cart.ts
@@ -0,0 +1,18 @@
+export interface CartState {
+  loading: boolean;
+  error: boolean;
+  totalAmount: number;
+  items: ICartItem[];
+
+  // Получение товаров из корзины
+  fetchCartItems: () => Promise<void>;
+
+  // Запрос на обновление количества товаров
+  updateItemQuantity: (id: number, quantity: number) => Promise<void>;
+
+  // Запрос на добавление товара в корзину
+  addCartItem: (values: CreateCartItemValues) => Promise<void>;
+
+  // Запрос на удаление товара из корзины
+  removeCartItem: (id: number) => Promise<void>;
+}
