849186b223075f52089b0ac97904f7769a91fb24
Vadim Cherepanov <xenos26@gmail.com>
2025-01-22 23:08:27 +0700

    feat(services): add update function in cart.ts

--- shared/services/cart.ts
+++ shared/services/cart.ts
@@ -5,4 +5,10 @@ export const getCart = async (): Promise<CartDTO> => {
   return (await axiosInstance.get<CartDTO>("/cart")).data;
 };
 
+export const updateItemQuantity = async (
+  itemId: number,
+  quantity: number
+): Promise<CartDTO> => {
+  return (await axiosInstance.patch<CartDTO>("/cart/" + itemId, { quantity }))
+    .data;
 };
