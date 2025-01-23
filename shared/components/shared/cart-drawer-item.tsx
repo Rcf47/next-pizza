61af83937ec944f6ded321ebb4c0c8644fc47777
Vadim Cherepanov <xenos26@gmail.com>
2025-01-23 19:33:06 +0700

    chore(shared): add onClickCountButton

--- shared/components/shared/cart-drawer-item.tsx
+++ shared/components/shared/cart-drawer-item.tsx
@@ -6,6 +6,7 @@ import { CountButton } from "@/shared/components/shared/count-button";
 import { Trash2Icon } from "lucide-react";
 
 interface Props extends CartItemProps {
+  onClickCountButton?: (type: "plus" | "minus") => void;
   className?: string;
 }
 
@@ -17,6 +18,7 @@ export const CartDrawerItem: React.FC<Props> = ({
   price,
   quantity,
   disabled,
+  onClickCountButton,
   className,
 }) => {
   return (
@@ -26,7 +28,7 @@ export const CartDrawerItem: React.FC<Props> = ({
         <CartItem.Info name={name} details={details} />
         <hr className="my-3" />
         <div className="flex items-center justify-between">
-          <CountButton onClick={(type) => console.log(type)} value={quantity} />
+          <CountButton onClick={onClickCountButton} value={quantity} />
           <div className="flex items-center gap-3">
             <CartItem.Price value={price} />
             <Trash2Icon
