71f9fd3cc5517a79acb28be0076d1e557ea3a9d3
Vadim Cherepanov <xenos26@gmail.com>
2025-01-24 22:05:27 +0700

    feat(services): add logic for addCartItem function

--- shared/services/cart.ts
+++ shared/services/cart.ts
@@ -1,5 +1,5 @@
 import { axiosInstance } from "@/shared/services/axios-instance";
-import { CartDTO } from "@/shared/services/dto/cart.dto";
+import { CartDTO, CreateCartItemValues } from "@/shared/services/dto/cart.dto";
 
 export const getCart = async (): Promise<CartDTO> => {
   return (await axiosInstance.get<CartDTO>("/cart")).data;
@@ -15,4 +15,10 @@ export const updateItemQuantity = async (
 
 export const removeCartItem = async (itemId: number): Promise<CartDTO> => {
   return (await axiosInstance.delete<CartDTO>("/cart/" + itemId)).data;
+};
+
+export const addCartItem = async (
+  values: CreateCartItemValues
+): Promise<CartDTO> => {
+  return (await axiosInstance.post<CartDTO>("/cart", values)).data;
 };
