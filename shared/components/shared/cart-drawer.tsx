ffc4e850f32d9ba3ce1cf319f492ff0bec50ff55
Vadim Cherepanov <xenos26@gmail.com>
2025-01-24 21:56:10 +0700

    fix(shared): fix margin-bottom in cart-drawer

--- shared/components/shared/cart-drawer.tsx
+++ shared/components/shared/cart-drawer.tsx
@@ -40,10 +40,9 @@ export const CartDrawer: React.FC<React.PropsWithChildren> = ({ children }) => {
           </SheetTitle>
         </SheetHeader>
         <div className="-mx-6 mt-5 overflow-auto flex-1">
-          <div className="mb-2">
-            {items.map((item) => (
+          {items.map((item) => (
+            <div className="mb-2" key={item.id}>
               <CartDrawerItem
-                key={item.id}
                 id={item.id}
                 imageUrl={item.imageUrl}
                 details={getCartItemDetails(
@@ -60,8 +59,8 @@ export const CartDrawer: React.FC<React.PropsWithChildren> = ({ children }) => {
                 }
                 onClickRemove={() => removeCartItem(item.id)}
               />
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
         <SheetFooter className="-mx-6 bg-white p-8">
           <div className="w-full">
