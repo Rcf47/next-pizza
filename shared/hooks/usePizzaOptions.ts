578c7b6732cf3f3a6c4119b68faaaaea8f94a083
Vadim Cherepanov <xenos26@gmail.com>
2025-01-27 23:14:05 +0700

    refactor(hooks): prettierd code in usePizzaOptions

--- shared/hooks/usePizzaOptions.ts
+++ shared/hooks/usePizzaOptions.ts
@@ -1,25 +1,24 @@
-import { Variant } from "@/shared/components/shared/group-variants";
 import { PizzaSize, PizzaType } from "@/shared/constants/pizza";
-import { getAvailablePizzaSizes } from "@/shared/lib/get-available-pizza-sizes";
-import { ProductItem } from "@prisma/client";
 import React from "react";
+import { Variant } from "../components/shared/group-variants";
 import { useSet } from "react-use";
+import { getAvailablePizzaSizes } from "../lib";
+import { ProductItem } from "@prisma/client";
 
 interface ReturnProps {
   size: PizzaSize;
   type: PizzaType;
-  setSize: (size: PizzaSize) => void;
-  setType: (type: PizzaType) => void;
   selectedIngredients: Set<number>;
-  addIngredient: (id: number) => void;
   availableSizes: Variant[];
   currentItemId?: number;
+  setSize: (size: PizzaSize) => void;
+  setType: (size: PizzaType) => void;
+  addIngredient: (id: number) => void;
 }
 
 export const usePizzaOptions = (items: ProductItem[]): ReturnProps => {
   const [size, setSize] = React.useState<PizzaSize>(20);
   const [type, setType] = React.useState<PizzaType>(1);
-
   const [selectedIngredients, { toggle: addIngredient }] = useSet(
     new Set<number>([])
   );
@@ -34,21 +33,21 @@ export const usePizzaOptions = (items: ProductItem[]): ReturnProps => {
     const isAvailableSize = availableSizes?.find(
       (item) => Number(item.value) === size && !item.disabled
     );
-    const availabelSize = availableSizes?.find((item) => !item.disabled);
+    const availableSize = availableSizes?.find((item) => !item.disabled);
 
-    if (!isAvailableSize && availabelSize) {
-      setSize(Number(availabelSize.value) as PizzaSize);
+    if (!isAvailableSize && availableSize) {
+      setSize(Number(availableSize.value) as PizzaSize);
     }
   }, [type]);
 
   return {
     size,
     type,
-    setSize,
-    setType,
     selectedIngredients,
-    addIngredient,
     availableSizes,
     currentItemId,
+    setSize,
+    setType,
+    addIngredient,
   };
 };
