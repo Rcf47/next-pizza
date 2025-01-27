647fccbc57b35cc43717bdf32ba1fe26fa14839d
Vadim Cherepanov <xenos26@gmail.com>
2025-01-27 23:03:31 +0700

    chore(components): changes in products-group-list

--- shared/components/shared/products-group-list.tsx
+++ shared/components/shared/products-group-list.tsx
@@ -1,26 +1,28 @@
 "use client";
 
-import { useIntersection } from "react-use";
-import { ProductCard } from "@/shared/components/shared/product-card";
-import { Title } from "@/shared/components/shared/title";
-import { cn } from "@/shared/lib/utils";
 import React from "react";
-import { useCategoryStore } from "@/shared/store/category";
+import { useIntersection } from "react-use";
+
+import { Title } from "./title";
+import { cn } from "@/shared/lib/utils";
+import { ProductCard } from "./product-card";
+import { useCategoryStore } from "@/shared/store";
+import { ProductWithRelations } from "@/@types/prisma";
 
 interface Props {
-  categoryId: number;
   title: string;
-  items: any[];
+  items: ProductWithRelations[];
+  categoryId: number;
   className?: string;
   listClassName?: string;
 }
 
 export const ProductsGroupList: React.FC<Props> = ({
-  categoryId,
   title,
   items,
-  className,
   listClassName,
+  categoryId,
+  className,
 }) => {
   const setActiveCategoryId = useCategoryStore((state) => state.setActiveId);
   const intersectionRef = React.useRef(null);
@@ -33,9 +35,11 @@ export const ProductsGroupList: React.FC<Props> = ({
       setActiveCategoryId(categoryId);
     }
   }, [categoryId, intersection?.isIntersecting, title]);
+
   return (
     <div className={className} id={title} ref={intersectionRef}>
       <Title text={title} size="lg" className="font-extrabold mb-5" />
+
       <div className={cn("grid grid-cols-3 gap-[50px]", listClassName)}>
         {items.map((product) => (
           <ProductCard
@@ -44,6 +48,7 @@ export const ProductsGroupList: React.FC<Props> = ({
             name={product.name}
             imageUrl={product.imageUrl}
             price={product.items[0].price}
+            ingredients={product.ingredients}
           />
         ))}
       </div>
