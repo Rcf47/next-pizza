0d41fd0dad0d3a6192fdaba277bee4b0b1aa6aa7
Vadim Cherepanov <xenos26@gmail.com>
2025-01-30 21:31:13 +0700

    refactor(components): comment sessionprovider in providers component

--- shared/components/shared/providers.tsx
+++ shared/components/shared/providers.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import { Toaster } from "react-hot-toast";
-import { SessionProvider } from "next-auth/react";
+//import { SessionProvider } from "next-auth/react";
 import { PropsWithChildren } from "react";
 
 export const Providers: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <>
-      <SessionProvider>{children}</SessionProvider>
+      <>{children}</>
       <Toaster />
     </>
   );
