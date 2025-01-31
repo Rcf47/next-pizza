d439f13567de79f02c76a058e61f75944c714f7e
Vadim Cherepanov <xenos26@gmail.com>
2025-01-31 19:42:02 +0700

    feat(components): add checkout-cart component

---
+++ shared/components/shared/checkout/checkout-cart.tsx
@@ -0,0 +1,55 @@
+import { WhiteBlock } from "@/shared/components/shared/white-block";
+import { PizzaSize, PizzaType } from "@/shared/constants/pizza";
+import { getCartItemDetails } from "@/shared/lib";
+import React from "react";
+
+interface Props {
+  items: CartStateItem[];
+  onClickCountButton: (
+    id: number,
+    quantity: number,
+    type: "plus" | "minus"
+  ) => void;
+  removeCartItem: (id: number) => void;
+  loading?: boolean;
+  className?: string;
+}
+
+export const CheckoutCart: React.FC<Props> = ({
+  items,
+  onClickCountButton,
+  removeCartItem,
+  loading,
+  className,
+}) => {
+  return (
+    <WhiteBlock title="1. Корзина" className={className}>
+      <div className="flex flex-col gap-5">
+        {loading
+          ? [...Array(4)].map((_, index) => (
+              <CheckoutItemSkeleton key={index} />
+            ))
+          : items.map((item) => (
+              <CheckoutItem
+                key={item.id}
+                id={item.id}
+                imageUrl={item.imageUrl}
+                details={getCartItemDetails(
+                  item.ingredients,
+                  item.pizzaType as PizzaType,
+                  item.pizzaSize as PizzaSize
+                )}
+                name={item.name}
+                price={item.price}
+                quantity={item.quantity}
+                disabled={item.disabled}
+                onClickCountButton={(type) =>
+                  onClickCountButton(item.id, item.quantity, type)
+                }
+                onClickRemove={() => removeCartItem(item.id)}
+              />
+            ))}
+      </div>
+    </WhiteBlock>
+  );
+};
