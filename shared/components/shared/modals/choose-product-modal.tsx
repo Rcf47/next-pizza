cb024b2ee5f4f71c4b88551fe83f917ebe0e460a
Vadim Cherepanov <xenos26@gmail.com>
2025-02-07 22:36:06 +0700

    feat(components): add visuallyhidden title and description

--- shared/components/shared/modals/choose-product-modal.tsx
+++ shared/components/shared/modals/choose-product-modal.tsx
@@ -3,11 +3,15 @@
 import { ProductWithRelations } from "@/@types/prisma";
 import { ProductForm } from "@/shared/components/shared/product-form";
 import { Dialog } from "@/shared/components/ui";
-import { DialogContent } from "@/shared/components/ui/dialog";
+import {
+  DialogContent,
+  DialogDescription,
+} from "@/shared/components/ui/dialog";
 import { cn } from "@/shared/lib/utils";
 import { DialogTitle } from "@radix-ui/react-dialog";
 import { useRouter } from "next/navigation";
 import React from "react";
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 
 interface Props {
   product: ProductWithRelations;
@@ -25,14 +29,16 @@ export const ChooseProductModal: React.FC<Props> = ({ product, className }) => {
           className
         )}
       >
-        <DialogTitle className="p-5 font-bold text-lg">
-          Выберите продукт
-        </DialogTitle>
-        {true ? (
-          <ProductForm product={product} onSubmit={() => router.back()} />
-        ) : (
-          <div>Hello</div>
-        )}
+        <VisuallyHidden asChild>
+          <DialogTitle className="p-5 font-bold text-lg">
+            Окно просмотра продукта
+          </DialogTitle>
+        </VisuallyHidden>
+        <VisuallyHidden asChild>
+          <DialogDescription>Выберите продукт</DialogDescription>
+        </VisuallyHidden>
+
+        <ProductForm product={product} onSubmit={() => router.back()} />
       </DialogContent>
     </Dialog>
   );
