cb76792cd18095ff30e37e431984ae2b3b8c313f
Vadim Cherepanov <xenos26@gmail.com>
2025-02-07 21:52:56 +0700

    refactor(lib): add space line in get-cart-details

--- shared/lib/get-cart-details.ts
+++ shared/lib/get-cart-details.ts
@@ -17,21 +17,24 @@ interface ReturnProps {
   items: CartStateItem[];
   totalAmount: number;
 }
+
 export const getCartDetails = (data: CartDTO): ReturnProps => {
-  const items = data.items.map((item) => ({
-    id: item.id,
-    quantity: item.quantity,
-    name: item.productItem.product.name,
-    imageUrl: item.productItem.product.imageUrl,
-    price: calcCartItemTotalPrice(item),
-    pizzaSize: item.productItem.size,
-    pizzaType: item.productItem.pizzaType,
-    disabled: false,
-    ingredients: item.ingredients.map((ingredient) => ({
-      name: ingredient.name,
-      price: ingredient.price,
-    })),
-  })) as CartStateItem[];
+  const items = data.items.map((item) => {
+    return {
+      id: item.id,
+      quantity: item.quantity,
+      name: item.productItem.product.name,
+      imageUrl: item.productItem.product.imageUrl,
+      price: calcCartItemTotalPrice(item),
+      pizzaSize: item.productItem.size,
+      pizzaType: item.productItem.pizzaType,
+      disabled: false,
+      ingredients: item.ingredients.map((ingredient) => ({
+        name: ingredient.name,
+        price: ingredient.price,
+      })),
+    };
+  }) as CartStateItem[];
 
   return {
     items,
