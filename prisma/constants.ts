99264d93ccdf4430c955e0ae666ef57a585171e7
Vadim Cherepanov <xenos26@gmail.com>
2025-02-12 23:41:03 +0700

    chore(prisma): change ingredients imageUrl in constants

--- prisma/constants.ts
+++ prisma/constants.ts
@@ -15,104 +15,87 @@ export const _ingredients = [
   {
     name: "Сливочная моцарелла",
     price: 79,
-    imageUrl:
-      "https://cdn.dodostatic.net/static/Img/Ingredients/cdea869ef287426386ed634e6099a5ba.png",
+    imageUrl: "/assets/images/ingredients/ingredient1.png",
   },
   {
     name: "Сыры чеддер и пармезан",
     price: 79,
-    imageUrl:
-      "https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA69C1FE796",
+    imageUrl: "/assets/images/ingredients/ingredient2.png",
   },
   {
     name: "Острый перец халапеньо",
     price: 59,
-    imageUrl:
-      "https://cdn.dodostatic.net/static/Img/Ingredients/11ee95b6bfdf98fb88a113db92d7b3df.png",
+    imageUrl: "/assets/images/ingredients/ingredient3.png",
   },
   {
     name: "Нежный цыпленок",
     price: 79,
-    imageUrl:
-      "https://cdn.dodostatic.net/static/Img/Ingredients/000D3A39D824A82E11E9AFA5B328D35A",
+    imageUrl: "/assets/images/ingredients/ingredient4.png",
   },
   {
     name: "Шампиньоны",
     price: 59,
-    imageUrl:
-      "https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA67259A324",
+    imageUrl: "/assets/images/ingredients/ingredient17.png",
   },
   {
     name: "Бекон",
     price: 79,
-    imageUrl:
-      "https://cdn.dodostatic.net/static/Img/Ingredients/000D3A39D824A82E11E9AFA637AAB68F",
+    imageUrl: "/assets/images/ingredients/ingredient5.png",
   },
   {
     name: "Ветчина",
     price: 79,
-    imageUrl:
-      "https://cdn.dodostatic.net/static/Img/Ingredients/000D3A39D824A82E11E9AFA61B9A8D61",
+    imageUrl: "/assets/images/ingredients/ingredient6.png",
   },
   {
     name: "Пикантная пепперони",
     price: 79,
-    imageUrl:
-      "https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA6258199C3",
+    imageUrl: "/assets/images/ingredients/ingredient7.png",
   },
   {
     name: "Острая чоризо",
     price: 79,
-    imageUrl:
-      "https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA62D5D6027",
+    imageUrl: "/assets/images/ingredients/ingredient8.png",
   },
   {
     name: "Маринованные огурчики",
     price: 59,
-    imageUrl:
-      "https://cdn.dodostatic.net/static/Img/Ingredients/000D3A21DA51A81211E9EA89958D782B",
+    imageUrl: "/assets/images/ingredients/ingredient9.png",
   },
   {
     name: "Свежие томаты",
     price: 59,
-    imageUrl:
-      "https://cdn.dodostatic.net/static/Img/Ingredients/000D3A39D824A82E11E9AFA7AC1A1D67",
+    imageUrl: "/assets/images/ingredients/ingredient10.png",
   },
   {
     name: "Красный лук",
     price: 59,
-    imageUrl:
-      "https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA60AE6464C",
+    imageUrl: "/assets/images/ingredients/ingredient11.png",
   },
   {
     name: "Сочные ананасы",
     price: 59,
-    imageUrl:
-      "https://cdn.dodostatic.net/static/Img/Ingredients/000D3A21DA51A81211E9AFA6795BA2A0",
+    imageUrl: "/assets/images/ingredients/ingredient12.png",
   },
   {
     name: "Итальянские травы",
     price: 39,
-    imageUrl:
-      "https://cdn.dodostatic.net/static/Img/Ingredients/370dac9ed21e4bffaf9bc2618d258734.png",
+    imageUrl: "/assets/images/ingredients/ingredient13.png",
   },
   {
     name: "Сладкий перец",
     price: 59,
-    imageUrl:
-      "https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA63F774C1B",
+    imageUrl: "/assets/images/ingredients/ingredient14.png",
   },
   {
     name: "Кубики брынзы",
     price: 79,
-    imageUrl:
-      "https://cdn.dodostatic.net/static/Img/Ingredients/000D3A39D824A82E11E9AFA6B0FFC349",
+    imageUrl: "/assets/images/ingredients/ingredient15.png",
   },
   {
     name: "Митболы",
     price: 79,
-    imageUrl:
-      "https://cdn.dodostatic.net/static/Img/Ingredients/b2f3a5d5afe44516a93cfc0d2ee60088.png",
+    imageUrl: "/assets/images/ingredients/ingredient16.png",
   },
 ].map((ingredient, index) => ({ id: index + 1, ...ingredient }));
 
