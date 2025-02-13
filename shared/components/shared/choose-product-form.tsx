7b15363b6b00439b83ff815ddf2eceba499c550b
Vadim Cherepanov <xenos26@gmail.com>
2025-02-13 22:44:27 +0700

    refactor(components): change img on Image in choose-product-form

--- shared/components/shared/choose-product-form.tsx
+++ shared/components/shared/choose-product-form.tsx
@@ -2,6 +2,7 @@ import { cn } from "@/shared/lib/utils";
 import React from "react";
 import { Title } from "./title";
 import { Button } from "../ui";
+import Image from "next/image";
 
 interface Props {
   imageUrl: string;
@@ -26,10 +27,12 @@ export const ChooseProductForm: React.FC<Props> = ({
   return (
     <div className={cn(className, "flex flex-1")}>
       <div className="flex items-center justify-center flex-1 relative w-full">
-        <img
+        <Image
           src={imageUrl}
           alt={name}
           className="relative left-2 top-2 transition-all z-10 duration-300 w-[350px] h-[350px]"
+          width={350}
+          height={350}
         />
       </div>
 
