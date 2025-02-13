52e2f1bec51da42e851e740534af0cef241a9795
Vadim Cherepanov <xenos26@gmail.com>
2025-02-13 22:41:10 +0700

    refactor(components): change img on Image in search-input

--- shared/components/shared/search-input.tsx
+++ shared/components/shared/search-input.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/shared/lib/utils";
 import { Api } from "@/shared/services/api-client";
 import { Product } from "@prisma/client";
 import { Search } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { useClickAway, useDebounce } from "react-use";
@@ -78,10 +79,12 @@ export const SearchInput: React.FC<Props> = ({ className }) => {
                 className="flex items-center gap-3 w-full px-3 py-2 hover:bg-primary/10"
                 href={`/product/${product.id}`}
               >
-                <img
+                <Image
                   className="rounded-sm h-8 w-8"
                   src={product.imageUrl}
                   alt={product.name}
+                  width={32}
+                  height={32}
                 />
                 <span>{product.name}</span>
               </Link>
