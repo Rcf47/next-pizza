d7e0267e99cb8674e0cc3396bbb054bab8b39741
Vadim Cherepanov <xenos26@gmail.com>
2025-02-13 22:44:49 +0700

    refactor(components): change img on Image in product-card

--- shared/components/shared/product-card.tsx
+++ shared/components/shared/product-card.tsx
@@ -4,6 +4,7 @@ import { Title } from "./title";
 import { Button } from "../ui";
 import { Plus } from "lucide-react";
 import { Ingredient } from "@prisma/client";
+import Image from "next/image";
 
 interface Props {
   id: number;
@@ -26,7 +27,13 @@ export const ProductCard: React.FC<Props> = ({
     <div className={className}>
       <Link href={`/product/${id}`}>
         <div className="flex justify-center p-6 bg-secondary rounded-lg h-[260px]">
-          <img className="w-[215px] h-[215px]" src={imageUrl} alt={name} />
+          <Image
+            className="w-[215px] h-[215px]"
+            src={imageUrl}
+            alt={name}
+            width={215}
+            height={215}
+          />
         </div>
 
         <Title text={name} size="sm" className="mb-1 mt-3 font-bold" />
